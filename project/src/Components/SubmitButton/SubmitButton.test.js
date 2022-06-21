7c0bc37a031610e404a298ab5f358b16bbb02fe3
ivonivanova <ivon.b.ivanova@gmail.com>
2022-06-21 14:06:04 +0100

    button test working

---
+++ project/src/Components/SubmitButton/SubmitButton.test.js
@@ -0,0 +1,10 @@
+import { test, expect, jest } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import SubmitButton from "./index.js"
+
+test('renders button', () => {
+    render(<SubmitButton />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+  
