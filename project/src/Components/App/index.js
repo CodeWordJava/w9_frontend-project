15a0366b6bf976a606fe6ee433f85f3ec7ff24fd
Luke <103851112+moeshy@users.noreply.github.com>
2022-06-21 15:31:06 +0100

    table

--- project/src/Components/App/index.js
+++ project/src/Components/App/index.js
@@ -5,17 +5,18 @@ import SubmitButton from "../SubmitButton";
 import DropDown from "../DropDown";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../NavBar/index.js"
-
+import TablePaginationActions from "../Display/index.js"
 
 function App() {
   return (
   <>
 <Navbar></Navbar>
-<img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
-<img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
-<img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
-<img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
-
+<div id="introDiv">
+  <p> Hello</p>
+</div>
+<div id="tableDiv">
+<TablePaginationActions></TablePaginationActions>
+</div>
 <Input></Input>
 <SubmitButton></SubmitButton>
 <DropDown></DropDown>
