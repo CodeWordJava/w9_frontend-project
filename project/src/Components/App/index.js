189ca9080772ffd3822ea8c13a5bb35477178e67
Luke <103851112+moeshy@users.noreply.github.com>
2022-06-24 12:02:25 +0100

    app comments

--- project/src/Components/App/index.js
+++ project/src/Components/App/index.js
@@ -1,40 +1,40 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Input from "../Input";
-// import SubmitButton from "../SubmitButton";
-import DropDown from "../DropDown";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "../NavBar/index.js"
-import CustomPaginationActionsTable from "../Display/index.js"
+import Navbar from "../NavBar/index.js";
+import CustomPaginationActionsTable from "../Display/index.js";
+
+//Rendering the App using our components
 
 function App() {
   return (
-  <>
-<Navbar></Navbar>
-<div id="introDiv">
-  <div id="innerIdDiv">
-  <p className="helloID">Student Resource Library</p>
-  <img src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"></img>
-  <div id="innerIntro">
-      <p>
-        Welcome to the School of Code Student Resourse Library! 
-        <br></br>
-        Here you can submit tech links from 
-        past presentations, guest speakers, or just anything you think might help your fellow bootcampers! We ask
-        that while you use this website that you take the time to upvote any links you feel really helped you
-         or links that really offer clear information you think might help others. Feel free to submit anything 
-         at the bottom of the page and happy hacking!
-        
-      </p>
-  </div>
-  </div> 
-</div>
-<div id="tableDiv">
-<CustomPaginationActionsTable></CustomPaginationActionsTable>
-</div>
-<Input></Input>
-{/* <SubmitButton></SubmitButton> */}
-</>
+    <>
+      <Navbar></Navbar>
+      <div id="introDiv">
+        <div id="innerIdDiv">
+          <p className="helloID">Student Resource Library</p>
+          <img src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"></img>
+          <div id="innerIntro">
+            <p>
+              Welcome to the School of Code Student Resourse Library!
+              <br></br>
+              Here you can submit tech links from past presentations, guest
+              speakers, or just anything you think might help your fellow
+              bootcampers! We ask that while you use this website that you take
+              the time to upvote any links you feel really helped you or links
+              that really offer clear information you think might help others.
+              Feel free to submit anything at the bottom of the page and happy
+              hacking!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="tableDiv">
+        <CustomPaginationActionsTable></CustomPaginationActionsTable>
+      </div>
+      <Input></Input>
+    </>
   );
 }
 
--- project/src/Components/Input/index.js
+++ project/src/Components/Input/index.js
@@ -3,53 +3,62 @@ import { useState } from "react";
 
 // This is our post fucntion that has input boxes for Topic Link and Username, and sends them to the API
 function Input() {
-    const [topic, setTopic] = useState("");
-    const [link, setLink] = useState("");
-    const [username, setUsername] = useState("");
+  const [topic, setTopic] = useState("");
+  const [link, setLink] = useState("");
+  const [username, setUsername] = useState("");
 
-//post request: handles form submits
-        async function handleSubmit(e) {
-            e.preventDefault();
-            console.log("submitted");
-            const post = await fetch("http://localhost:9000/create", {
-            method: 'POST',
-            headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            body: JSON.stringify({topic: topic, link: link, userName: username})
+  //post request: handles form submits
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted");
+    const post = await fetch("http://localhost:9000/create", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ topic: topic, link: link, userName: username }),
+    });
+    const res = await post.json();
+    console.log(res);
+  }
 
-        })
-          const res = await post.json()
-            console.log(res)
-        };
-       
-    
-      function handleTopicChange(e) {
-        setTopic(e.target.value);
-      }
-      function handleLinkChange(e) {
-        setLink(e.target.value);
-      }
-    
-      function handleUsernameChange(e) {
-        setUsername(e.target.value);
-      }
+  function handleTopicChange(e) {
+    setTopic(e.target.value);
+  }
+  function handleLinkChange(e) {
+    setLink(e.target.value);
+  }
 
-    return (
-      <div id="formDiv">
-        <form id="form" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Topic" onChange={handleTopicChange} required> 
-            </input>
-            <input type="text" placeholder="Link" onChange={handleLinkChange} required> 
-            </input>
-            <input type="text" placeholder="Username" onChange={handleUsernameChange} required> 
-            </input>
-            <input id="sumbutButton" type="submit" value="Submit Link"></input>
-        </form>
-      </div>
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
 
-    )
+  return (
+    <div id="formDiv">
+      <form id="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Topic"
+          onChange={handleTopicChange}
+          required
+        ></input>
+        <input
+          type="text"
+          placeholder="Link"
+          onChange={handleLinkChange}
+          required
+        ></input>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={handleUsernameChange}
+          required
+        ></input>
+        <input id="sumbutButton" type="submit" value="Submit Link"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Input;
