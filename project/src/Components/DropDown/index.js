a9c54f4afad557389880d2842c6954aa6bcce6a8
Luke <103851112+moeshy@users.noreply.github.com>
2022-06-24 12:06:20 +0100

    tidy dropdown

--- project/src/Components/DropDown/index.js
+++ project/src/Components/DropDown/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 
+//This is passing in the setTopic prop to get a value defined by the onchange.
 
-
-function DropDown({setTopic}) {
-    return (
-        <div>
-        <select id="dropDown" onChange = {e => setTopic(e.target.value)}>
-          <option value="">filter by topic</option>
-          <option value="Git">Git</option>
-          <option value="react">React</option>
-          <option value="NodeJS">NodeJS</option>
-          <option value="SQL">SQL</option>
-          <option value="JavaScript">JavaScript</option>
-          <option value="CSS">CSS</option>
-        </select>
-      </div>
-    )
+function DropDown({ setTopic }) {
+  return (
+    <div>
+      <select id="dropDown" onChange={(e) => setTopic(e.target.value)}>
+        <option value="">filter by topic</option>
+        <option value="Git">Git</option>
+        <option value="react">React</option>
+        <option value="NodeJS">NodeJS</option>
+        <option value="SQL">SQL</option>
+        <option value="JavaScript">JavaScript</option>
+        <option value="CSS">CSS</option>
+      </select>
+    </div>
+  );
 }
 
 export default DropDown;
