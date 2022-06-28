9d81dcb7c21e08666e34a451787b301f6e4bad4b
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-06-28 15:21:02 +0100

    readme added to help explain Display component

--- project/cypress/integration/sample_spec.js
+++
@@ -1,8 +0,0 @@
-<reference types='cypress'/>
-
-describe('Form Submit', () => {
-    it('will test the input boxes', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('input').type('hello')
-    })
-})
